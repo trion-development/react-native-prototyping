9ed801ee3dc26abca57ff73fb69884e94ec9f7e7
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-04-18 23:00:33 +0200

    Anpassung des Textes und der Hintergrundfarbe

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hallo Welt Test 123</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d3fccf',
     alignItems: 'center',
     justifyContent: 'center',
   },
