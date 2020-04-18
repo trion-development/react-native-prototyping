667a4e1e1fe0963aaf45fa2ac6af72398ffa56d9
Steffen Jacobs <steffen.jacobs@hotmail.de>
2020-04-18 23:03:22 +0200

    Ausbau der App mit State

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default class App extends React.Component<any, any> {
+interface AppState {
+  locationText: string
+  editedLocationText?: string
+}
 
-  constructor(props: any){
-    super(props);
+export default class App extends React.Component<any, AppState> {
+
+  constructor(args: any) {
+    super(args);
+    this.state = { locationText: "Hallo Welt!" };
+  }
+
+  private saveLocation() {
+    console.log(`Aktualisiere Ort: ${this.state.editedLocationText}`);
+    this.setState({ locationText: this.state.editedLocationText ? this.state.editedLocationText : "", editedLocationText: "" });
+  }
+
+  private onLocationTextChange(text: string) {
+    this.setState({ editedLocationText: text });
   }
 
   public render() {
     return (
       <View style={styles.container}>
-        <Text>Hallo Welt Test 123</Text>
+        <Text>{this.state.locationText}</Text>
+        <TextInput placeholder="Ort eingeben" onChangeText={(text) => this.onLocationTextChange(text)} />
+        <Button title="Übernehmen" onPress={() => this.saveLocation()} />
       </View>
     );
   }
